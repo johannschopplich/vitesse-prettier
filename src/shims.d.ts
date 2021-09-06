77309a8a0698c1be9486454c220bd60a26fd038b
Johann Schopplich <mail@johannschopplich.com>
2021-09-06 09:51:16 +0200

    refactor: vue shim types

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,13 +1,12 @@
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 declare interface Window {
   // extend the window
+}
+
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  const component: ComponentOptions
+  export default component
 }
 
 // with vite-plugin-md, markdowns can be treat as Vue components
