4a4681d1dacbb0cdf09771281049a8ef6d08f273
Johann Schopplich <mail@johannschopplich.com>
2021-09-13 10:58:56 +0200

    refactor: type script shims

---
+++ src/env.d.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+declare interface Window {
+  // extend the window
+}
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// with vite-plugin-md, markdowns can be treat as Vue components
+declare module '*.md' {
+  import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
