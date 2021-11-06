e9a54087e679fa67d6896c78f205d105a3beaab6
Johann Schopplich <mail@johannschopplich.com>
2021-11-06 08:43:49 +0100

    feat: handle relative links in md semantically

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -4,38 +4,68 @@
 
   <div class="py-4" />
 
-  <slot />
+  <section ref="content">
+    <slot />
+  </section>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { isClient, useEventListener } from '@vueuse/core'
+import { useRouter } from 'vue-router'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const props = defineProps<{ frontmatter: any }>()
 
-const navigate = () => {
-  if (window.location.hash) {
-    document
-      .querySelector(decodeURIComponent(window.location.hash))
-      ?.scrollIntoView({ behavior: 'smooth' })
-  }
-}
+const content = ref<HTMLElement>()
+const router = useRouter()
 
 if (isClient) {
+  const navigate = () => {
+    if (window.location.hash) {
+      document
+        .querySelector(decodeURIComponent(window.location.hash))
+        ?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  const handleAnchors = (
+    event: MouseEvent & {
+      target: HTMLElement
+    }
+  ) => {
+    const link = event.target.closest('a')
+
+    if (
+      !event.defaultPrevented &&
+      link &&
+      event.button === 0 &&
+      link.target !== '_blank' &&
+      link.rel !== 'external' &&
+      !link.download &&
+      !event.metaKey &&
+      !event.ctrlKey &&
+      !event.shiftKey &&
+      !event.altKey
+    ) {
+      const url = new URL(link.href)
+      if (url.origin !== window.location.origin) return
+
+      event.preventDefault()
+      const { hash, pathname: path } = url
+      if (hash && !path) {
+        window.history.replaceState({}, '', hash)
+        navigate()
+      } else {
+        router.push({ path, hash })
+      }
+    }
+  }
+
   useEventListener(window, 'hashchange', navigate)
+  useEventListener(content, 'click', handleAnchors)
 
   onMounted(() => {
-    for (const anchor of document.querySelectorAll<HTMLAnchorElement>(
-      'a[href^="#"]'
-    )) {
-      anchor.addEventListener('click', (evt) => {
-        evt.preventDefault()
-        window.history.replaceState({}, '', anchor.href)
-        navigate()
-      })
-    }
-
     navigate()
     setTimeout(navigate, 500)
   })
