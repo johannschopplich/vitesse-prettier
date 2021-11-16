c796a7f4e8d0c23952ba6e2638b4466acc7ee621
Johann Schopplich <mail@johannschopplich.com>
2021-11-16 16:21:08 +0100

    chore: upgrade dependencies

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -1,13 +1,13 @@
 import type { UserModule } from '~/types'
 
 // https://vite-plugin-pwa.netlify.app
-export const install: UserModule = ({ isClient, router }) => {
+// Only needed with `registerType: 'autoUpdate'`,
+// not for reloading prompt
+export const __deactivated_install: UserModule = ({ isClient, router }) => {
   if (!isClient) return
 
   router.isReady().then(async () => {
-    // Only needed with `registerType: 'autoUpdate'`,
-    // not for reloading prompt:
-    // const { registerSW } = await import('virtual:pwa-register')
-    // registerSW({ immediate: true })
+    const { registerSW } = await import('virtual:pwa-register')
+    registerSW({ immediate: true })
   })
 }
